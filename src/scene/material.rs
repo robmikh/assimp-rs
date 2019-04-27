e64a39a1d7cc4afef8300ecc344ee9b38384123a
robmikh <rob.mikh@outlook.com>
2019-04-27 00:14:45 -0700

    move to type specific property accessors

--- src/scene/material.rs
+++ src/scene/material.rs
@@ -1,6 +1,9 @@
-use ffi::{AiMaterial, AiMaterialProperty};
+use std::ffi::CString;
+use std::os::raw::{c_uint};
+use ffi::{AiMaterial, AiMaterialProperty, AiPropertyTypeInfo, AiColor4D, aiGetMaterialColor, AiReturn};
 
 use super::material_property::{MaterialProperty, MaterialPropertyIter};
+use math::color3::Color3D;
 
 define_type_and_iterator_indirect! {
     /// Material type (not yet implemented)
@@ -28,12 +31,24 @@ impl<'a> Material<'a> {
         }
     }
 
-    pub fn get_property_by_key(&self, key: &str) -> Option<MaterialProperty> {
-        for property in self.property_iter() {
-            if property.key() == key {
-                return Some(property);
-            }
+    pub fn get_color(&self, key: &str) -> Option<Color3D> {
+        let cstr = CString::new(key).unwrap();
+
+        let mut raw_color = AiColor4D { a: 0.0, r: 0.0, g: 0.0, b: 0.0 };
+        let result = unsafe {
+            aiGetMaterialColor(
+                self.0, 
+                cstr.as_ptr(), 
+                AiPropertyTypeInfo::Float as c_uint, 
+                0 as c_uint, 
+                &mut raw_color)
+        };
+
+        match result {
+            AiReturn::Success => {
+                Some(Color3D::new(raw_color.r, raw_color.g, raw_color.b))
+            },
+            _ => None,
         }
-        None
     }
 }
